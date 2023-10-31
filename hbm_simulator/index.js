8eda328735a436819bc8a474363d89b378fee847
Henrique Rezende <henrique.64@hotmail.com>
2023-10-31 18:34:28 -0300

    feat: calling grpc with generated data

--- hbm_simulator/index.js
+++ hbm_simulator/index.js
@@ -1,5 +1,26 @@
+const { clientCall } = require("./src/client");
+
 async function run() {
-  console.log("Start");
+  let x = 0;
+  const step = 1; // Update every 1 millisecond
+
+  function generateData() {
+    const y =
+      -0.06366 +
+      0.12613 * Math.cos((Math.PI * x) / 500) +
+      0.12258 * Math.cos((Math.PI * x) / 250) +
+      0.01593 * Math.sin((Math.PI * x) / 500) +
+      0.03147 * Math.sin((Math.PI * x) / 250);
+
+    const data = { x, y };
+
+    x += step;
+    return data;
+  }
+
+  setInterval(() => {
+    clientCall.write({ hbmData: generateData() });
+  }, step);
 }
 
 run().catch(console.error);
