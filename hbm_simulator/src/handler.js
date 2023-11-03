1cd7e7333a531ae933fcfb0dfe3ccb5f3f8ba3ed
Henrique Rezende <henrique.64@hotmail.com>
2023-11-03 14:25:21 -0300

    feat: add startIrregularMeasurement on handler

--- hbm_simulator/src/handler.js
+++ hbm_simulator/src/handler.js
@@ -1,34 +1,45 @@
 const { client } = require("./client");
 
 let measurementInterval = null;
+let milisecond = 0;
+const MEASURE_MILISECOND_RATE = 100;
 
-function startNormalMeasurement(call, callback) {
-  let milisecond = 0;
-  const step = 100; // Update every x millisecond
+function generateData(willGenerateIrregular) {
+  let milivolt =
+    -0.06366 +
+    0.12613 * Math.cos((Math.PI * milisecond) / 500) +
+    0.12258 * Math.cos((Math.PI * milisecond) / 250) +
+    0.01593 * Math.sin((Math.PI * milisecond) / 500) +
+    0.03147 * Math.sin((Math.PI * milisecond) / 250);
 
-  function generateData() {
-    let milivolt =
-      -0.06366 +
-      0.12613 * Math.cos((Math.PI * milisecond) / 500) +
-      0.12258 * Math.cos((Math.PI * milisecond) / 250) +
-      0.01593 * Math.sin((Math.PI * milisecond) / 500) +
-      0.03147 * Math.sin((Math.PI * milisecond) / 250);
-
-    //generate random irregular values
-    const randomInteger = Math.floor(Math.random() * 10);
-    if (randomInteger > 8) {
+  if (willGenerateIrregular) {
+    const randomInteger = Math.floor(Math.random() * 100);
+    if (randomInteger > 95) {
       milivolt = milivolt * 1.3;
     }
-    //
-
-    const hbmData = { milisecond, milivolt };
-    milisecond += step;
-    return hbmData;
   }
 
+  const hbmData = { milisecond, milivolt };
+  milisecond += MEASURE_MILISECOND_RATE;
+  return hbmData;
+}
+
+function startNormalMeasurement(call) {
+  milisecond = 0;
+
   measurementInterval = setInterval(() => {
-    client.sendHbmData().write({ hbmData: generateData() });
-  }, step);
+    client.sendHbmData().write({ hbmData: generateData(false) });
+  }, MEASURE_MILISECOND_RATE);
+
+  call.end();
+}
+
+function startIrregularMeasurement(call) {
+  milisecond = 0;
+
+  measurementInterval = setInterval(() => {
+    client.sendHbmData().write({ hbmData: generateData(true) });
+  }, MEASURE_MILISECOND_RATE);
 
   call.end();
 }
@@ -39,12 +50,12 @@ function stopMeasurement(call) {
 }
 
 function sendIrregularityAlert(call) {
-  console.log("VAAAAAAAI");
-  console.log(call.request);
+  console.log(call.request.message);
 }
 
 module.exports = {
   startNormalMeasurement,
+  startIrregularMeasurement,
   stopMeasurement,
   sendIrregularityAlert,
 };
